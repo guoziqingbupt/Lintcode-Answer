8f4ba087bb03d7cfb5391e9541d8bb4042628c4d
guoziqingbupt <guoziqing@bupt.edu.cn>
2018-03-28 22:12:03 +0800

    27

---
+++ reverse array.py
@@ -0,0 +1,10 @@
+class Solution:
+    """
+    @param nums: a integer array
+    @return: nothing
+    """
+    def reverseArray(self, nums):
+        nums.reverse()
+        return nums
+        # write your code here
+
