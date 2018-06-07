e24e446f51453c65be8d9a651f9cbf845000a997
guoziqingbupt <guoziqing@bupt.edu.cn>
2018-06-07 21:01:14 +0800

    86

---
+++ binary tree preorder traversal.py
@@ -0,0 +1,29 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        result = []
+
+        if not root:
+            return result
+
+        stack = [root]
+        while len(stack) != 0:
+            cur = stack.pop()
+            result.append(cur.val)
+            if cur.right:
+                stack.append(cur.right)
+            if cur.left:
+                stack.append(cur.left)
+        return result
+        # write your code here
